06252517b5f7151ac37b8c9e61e3ae84e5ef19b5
dina-r2o2 <r2o2@rambler.ru>
2016-05-15 12:10:07 +0300

    js доработка

--- js/script.js
+++ js/script.js
@@ -1,23 +1,8 @@
 var link = document.querySelector(".btn-search");
-var form = document.querySelector(".index-search");
-var date_checkin = form.querySelector("[name=checkin]");
-var date_checkout = form.querySelector("[name=checkout]");
-var guest_adult = form.querySelector("[name=adult]");
-var guest_child = form.querySelector("[name=child]");
+var popup = document.querySelector(".index-search");
+
 
 link.addEventListener("click", function(event) {
     event.preventDefault();
-    form.classList.toggle("index-search-show");
-    date_checkin.focus();
-});
-
-form.addEventListener("submit", function(event) {
-  if (!date_checkin.value || !date_checkout.value) {
-    event.preventDefault();
-    form.classList.add("index-search-error");
-  }
-  if (!guest_adult.value || !guest_child.value) {
-    event.preventDefault();
-    form.classList.add("index-search-error");
-  }
+    popup.classList.toggle("index-search-show");
 });
